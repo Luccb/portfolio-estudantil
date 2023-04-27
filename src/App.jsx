65305165cbad46d3ef5e419b47cfd213d1e29d35
LUCIANO CASTRO BORGES <0057846@IFMG.EDU.BR>
2023-04-27 17:41:53 -0300

    prototipo... a ver conponent collapse

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import fundoMatrix from './assets/fundo-matrix.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [foldSobre, setFoldSobre] = useState(0);
+  const [foldHabilidades, setFoldHabilidades] = useState(0);
+  const [foldExperiencia, setFoldExperiencia] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
+      <div className="topo">
+        <h1>Desenvolvedor Full-Stack</h1>
+        
+        <div className="cardPessoal">
+          <div className="identificacao">
+            <div className="foto"></div>
+            <p></p>
+          </div>
+          <div></div>
+        </div>
+
+        <div className="descricao">
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+
+
+
+      <div>
+
+      </div>
+      
+      <div className="conteudo">
+        <button onClick={() => setFoldSobre(() => foldSobre = foldSobre ? 0 : 1 )}>
+          {foldSobre ? '+' : '-'}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <span className={`
+            {foldSobre ? 'folded' : 'expanded'}    
+          
+          `}>
+            
+          </span>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      
+      <div className="footer">
+
+      </div>
+
+
     </div>
   )
 }
